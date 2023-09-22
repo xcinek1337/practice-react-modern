24c9617935bf02a6f05f77e1f6fbfc3100bee9ed
Marcin <xcinek1337@wp.pl>
2023-09-22 22:49:29 +0200

    mine version 04

--- 04/SpeedTest.js
+++ 04/SpeedTest.js
@@ -1,17 +1,64 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import useRandomItem from './hook';
 
 const SpeedTest = () => {
     const [word, regenerateWord] = useRandomItem(['devmentor.pl', 'abc', 'JavaScript']);
+    const [correctWordsCount, setCorrectWordsCount] = useState(0);
+
+    const [intervalId, setIntervalId] = useState(null);
+    const [seconds, setSeconds] = useState(0);
+    const [milliseconds, setMilliseconds] = useState(0);
+
+    const [inputValue, setInputValue] = useState('');
+
+    const capturing = (e) => {
+        const newValue = e.target.value;
+        setInputValue(newValue);
+    };
 
     useEffect(() => {
         regenerateWord();
     }, []);
 
+    const appRunning = () => {
+        const id = setInterval(() => {
+            setMilliseconds((prevMilliseconds) => (prevMilliseconds + 10) % 100);
+            setSeconds((prevTime) => prevTime + 1);
+        }, 100);
+        setIntervalId(id);
+    };
+   
+    const appStop = () => {
+        clearInterval(intervalId);
+    };
+
+    const plainSeconds = Math.floor(seconds / 10);
+
+    const processingApp = (e) => {
+        if (e.key === 'Enter' && inputValue === word) {
+            setCorrectWordsCount((count) => count + 1);
+            setInputValue('');
+        }
+    };
+
+    useEffect(() => {
+        if (correctWordsCount === 3) {
+            appStop();
+        }
+    });
+
     return (
         <div>
             <h1>{word}</h1>
-            <input />
+            <input
+                onFocus={appRunning}
+                value={inputValue}
+                onChange={capturing}
+                onKeyDown={processingApp}
+            />
+            <p>
+                Czas: {plainSeconds}:{milliseconds}
+            </p>
         </div>
     );
 };
--- 04/app.js
+++ 04/app.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import SpeedTest from './SpeedTest';
 
-const App = () => {
-    return <SpeedTest />;
-};
+const App = () => (
+    
+    <SpeedTest />
+);
 
 const root = createRoot(document.querySelector('#root'));
 root.render(<App />);
