9ad1e329749e63691eddcef4cca29f3aff2f5fef
Marcin <xcinek1337@wp.pl>
2023-09-24 17:46:31 +0200

    05 extra

--- 05/app.js
+++ 05/app.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
-import ContactForm from './ContactForm';
+// eslint-disable-next-line import/named
+import  ContactForm  from './ContactForm';
 
-const App = () => <ContactForm />;
+const App = () => (
+    <ContactForm />
+);
 
 const root = createRoot(document.querySelector('#root'));
 root.render(<App />);
