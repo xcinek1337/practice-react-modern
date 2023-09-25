5e6d684f89893687ea584cf4b9e386b53e55e70a
Marcin <xcinek1337@wp.pl>
2023-09-25 11:58:48 +0200

    idk

--- 05/ContactForm.js
+++ 05/ContactForm.js
@@ -1,12 +1,43 @@
-/* eslint-disable no-console */
+/* eslint-disable */
+
 import React, { useReducer, useRef } from 'react';
-import emailjs from '@emailjs/browser';
-import { SERVICE_ID, TEMPLATE_ID, PUBLIC_KEY } from './account';
 
 const ContactForm = () => {
     const form = useRef();
 
-    const init = {
+    const initFields = [
+        {
+            name: 'firstName',
+            label: 'Imię',
+            required: true,
+            pattern: '^[a-zA-Z –-]+$',
+        },
+        {
+            name: 'email',
+            label: 'Email',
+            required: true,
+            pattern: '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,4}$',
+        },
+        {
+            name: 'phone',
+            type: 'number',
+            label: 'Numer Telefonu',
+            required: false,
+            pattern: '/^d{9}$',
+        },
+        {
+            name: 'subject',
+            label: 'Temat wiadomośći',
+            required: true,
+        },
+        {
+            name: 'textArea',
+            label: 'Wiadomość',
+            required: true,
+        },
+    ];
+
+    const emptyFields = {
         firstName: '',
         email: '',
         phone: '',
@@ -15,162 +46,114 @@ const ContactForm = () => {
         errors: {
             firstName: '',
             email: '',
+            phone: '',
             subject: '',
             textArea: '',
         },
     };
-    const isValidEmail = (email) => {
-        const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-        return emailRegex.test(email);
-    };
-    // eslint-disable-next-line no-unused-vars
-    const isValidPhone = (phone) => {
-        const phoneRegex = /^\d{9}$/;
-        return phoneRegex.test(phone);
-    };
 
-    const validateFields = (values) => {
+    const validateFields = (name, value) => {
         const errors = {};
 
-        if (values.firstName.trim() === '') {
-            errors.firstName = 'Pole Imię nie może być puste';
+        const field = initFields.find((field) => field.name === name);
+
+        if (!field) {
+            return errors;
         }
 
-        if (!isValidEmail(values.email)) {
-            errors.email = 'Niepoprawny format adresu e-mail';
+        const { label, required, pattern } = field;
+
+        if (required && value.trim() === '') {
+            errors[name] = `Pole ${label} jest wymagane.`;
         }
-        if (values.subject.trim() === '') {
-            errors.subject = 'Temat wiadomosci jest obowiazkowy';
-        }
-        if (!isValidPhone(values.phone) && values.phone.length > 0) {
-            errors.phone = 'Nieprawidłowa długość znaków';
-        }
-        if (values.textArea.trim() === '') {
-            errors.textArea = 'wiadomosc nie moze być pusta';
+        if (pattern && value.trim() !== '') {
+            const reg = new RegExp(pattern);
+            if (!reg.test(value)) {
+                errors[name] = `Dane w polu ${label} są niepoprawne.`;
+            }
         }
 
         return errors;
     };
 
-    
-    // eslint-disable-next-line consistent-return
     const reducer = (state, { name, value, action = 'change' }) => {
         switch (action) {
-        case 'change': {
-            const newState = { ...state, [name]: value };
-            newState.errors = validateFields(newState);
-            return newState;
-        }
-        case 'reset': {
-            return init
-        }
-        // eslint wymusza default...
-        default: {
-            console.log('default');
-        }
+            case 'change': {
+                const newState = { ...state, [name]: value };
+                newState.errors = validateFields(newState);
+                // newState.errors = { ...newState.errors, [name]: validateFields(name, value) };
+                return newState;
+            }
+            case 'reset': {
+                return emptyFields;
+            }
+            default: {
+                return state;
+            }
         }
     };
 
-    const [state, dispatch] = useReducer(reducer, init);
+    const [state, dispatch] = useReducer(reducer, emptyFields);
     const { firstName, email, errors, phone, subject, textArea } = state;
 
     const submit = (e) => {
         e.preventDefault();
 
+    
         const updatedErrors = validateFields(state);
-        dispatch({ name: 'errors', value: updatedErrors });
 
         if (Object.values(updatedErrors).every((error) => error === '')) {
-            emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, form.current, PUBLIC_KEY).then(
-                (result) => {
-                    console.log(result.text);
-                    console.log('message sent');
-                },
-                (error) => {
-                    console.log(error.text);
-                },
-            );
+      
+            // emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, form.current, PUBLIC_KEY).then(
+            //     (result) => {
+            //         console.log(result.text);
+            //         console.log('message sent');
+            //     },
+            //     (error) => {
+            //         console.log(error.text);
+            //     },
+            // );
             // eslint-disable-next-line no-alert
-            window.alert('thanks for message');
+         
+            console.log(`formularz wysłany`);
 
-            dispatch({ action: 'reset' });
+            // dispatch({ action: 'reset' });
+        } else {
+        
+            console.log('else');
+            dispatch({ name: 'errors', value: updatedErrors });
         }
     };
 
     return (
         <form ref={form} noValidate onSubmit={submit}>
-            <div>
-                <label htmlFor="firstName">
-                    Imię:
-                    <input
-                        type="text"
-                        id="firstName"
-                        name="firstName"
-                        required
-                        value={firstName}
-                        onChange={(e) => dispatch(e.target)}
-                    />
-                </label>
-
-                {errors.firstName && <p style={{ color: 'red' }}>{errors.firstName}</p>}
-            </div>
-
-            <div>
-                <label htmlFor="email">
-                    email:
-                    <input
-                        type="email"
-                        id="email"
-                        name="email"
-                        required
-                        value={email}
-                        onChange={(e) => dispatch(e.target)}
-                    />
-                </label>
-                {errors.email && <p style={{ color: 'red' }}>{errors.email}</p>}
-            </div>
-
-            <div>
-                <label htmlFor="phone">
-                    telefon:
-                    <input
-                        type="number"
-                        id="phone"
-                        name="phone"
-                        value={phone}
-                        onChange={(e) => dispatch(e.target)}
-                    />
-                </label>
-                {errors.phone && <p style={{ color: 'red' }}>{errors.phone}</p>}
-            </div>
-            <div>
-                <label htmlFor="subject">
-                    temat wiadomosci:
-                    <input
-                        type="text"
-                        id="subject"
-                        name="subject"
-                        required
-                        value={subject}
-                        onChange={(e) => dispatch(e.target)}
-                    />
-                </label>
-                {errors.subject && <p style={{ color: 'red' }}>{errors.subject}</p>}
-            </div>
-            <div>
-                <label htmlFor="textArea">
-                    <textarea
-                        value={textArea}
-                        onChange={(e) => dispatch(e.target)}
-                        name="textArea"
-                        id="textArea"
-                        cols="30"
-                        rows="10"
-                    />
-                </label>
-                {errors.textArea && <p style={{ color: 'red' }}>{errors.textArea}</p>}
-            </div>
-
+            {initFields.map((field) => (
+                <div key={field.name}>
+                    <label htmlFor={field.name}>
+                        {field.label}:
+                        {field.name === 'textArea' ? (
+                            <textarea
+                                type="text"
+                                id={field.name}
+                                name={field.name}
+                                required={field.required}
+                                value={state[field.name]}
+                                onChange={(e) => dispatch(e.target)}
+                            />
+                        ) : (
+                            <input
+                                id={field.name}
+                                name={field.name}
+                                type={field.type}
+                                required={field.required}
+                                value={state[field.name]}
+                                onChange={(e) => dispatch(e.target)}
+                            />
+                        )}
+                    </label>
+                    {errors[field.name] && <p style={{ color: 'red' }}>{errors[field.name]}</p>}
+                </div>
+            ))}
             <input type="submit" />
         </form>
     );
